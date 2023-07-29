4474f50102dca3a752553c6b76ca547a51f4a840
Alperen Çorak <alperen_corak@outlook.com>
2023-07-29 17:19:40 +0200

    Clean Blog Template Engine Task Completed

--- CleanBlog_Task/app.js
+++ CleanBlog_Task/app.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
+// EJS Added
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  const blog = {
-    id: 1,
-    title: 'Blog Title',
-    description: 'Blog DesC',
-  };
-  res.send(blog);
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
 });
 
 const port = 7096;
